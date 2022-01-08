a0eb0d7fc5ed9695ec0da4d6472cacc8bd828bfb
ale_figure8 <gomeznieto@gmail.com>
2022-01-08 08:14:33 -0300

    pagination fix bug

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -12,15 +12,15 @@ const Pagination = ({next, prev}) => {
 
     return (
         <div className='flex justify-center mt-5'>
-            {prev !== null && <button onClick={() => {
-                dispatch(paginationCharacter(prev))
-                topFunction()
+            {prev !== null && <button onClick={async() => {
+                await dispatch(paginationCharacter(prev))
+               await topFunction()
             }} 
                 className='text-zinc-200 uppercase font-semibold bg-zinc-800 p-3 rounded-l-md hover:bg-zinc-600 duration-300' 
             >Prev</button>}
-            {next !== null && <button onClick={() => {
-                dispatch(paginationCharacter(next))
-                topFunction()
+            {next !== null && <button onClick={async() => {
+                await dispatch(paginationCharacter(next))
+                await topFunction()
             }} 
                 className='text-zinc-200 uppercase font-semibold bg-zinc-800 p-3 rounded-r-md hover:bg-zinc-600 duration-300' 
             >Next</button>}
